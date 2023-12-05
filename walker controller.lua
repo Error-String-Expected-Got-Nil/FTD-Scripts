5c423efcd8b66c4d830bca5d0dea46f1686afce9
Error-String-Expected-Got-Nil <103805191+Error-String-Expected-Got-Nil@users.noreply.github.com>
2023-12-05 15:15:26 -0600

    Update walker controller.lua
    Update walker controller.lua
    
    More config
    
    Delta cap config
    
    Improve comments
    
    Change TODO

--- walker controller.lua
+++ walker controller.lua
@@ -7,6 +7,7 @@
 --  -Add custom axis "splayLegs", stopping all leg motion and splaying the legs if it is at 1
 --  -Add strafe axis
 --  -Make yaw axis cause legs to move tangent to center of mass
+--  -Automatically determine leg segment lengths
 
 deg = Mathf.Rad2Deg
 rad = Mathf.Deg2Rad
@@ -14,33 +15,42 @@ pi2 = Mathf.PI * 2
 startupLog = ""
 function logBuffer(str) startupLog = startupLog .. str .. "\n" end
 
---###########################################################################################
---#                                                                                         #
---#   Spinblocks should be oriented such that clockwise (positive rotation) means upwards   #
---#         (or their global drive response set to -1 if they are counterclockwise)         #
---#                                                                                         #
---#   Additionally, all spinblocks should be in "Rotate at a speed determined by the rate   #
---#                  controller" mode, with rotation speed set to maximum.                  #
---#                                                                                         #
---###########################################################################################
-
-config = {
-    stepAngle = 30;             --Farthest angle, in degrees, a leg should move away from its default angle while moving.
-    cycleDuration = 2;          --Amount of time, in seconds, it takes for a single walk cycle to pass.
-    deltaTime = 1/40;           --Amount of time, in seconds, that passes each tick. Should be 1/40th of a second, the length of an FTD physics step.
-    walkDriveDeltaCap = 1/40;   --Maximum amount a leg's walking drive response (main, forward, yaw) can change in a single tick. Exists to smooth out jerky motion.
-    heightDriveDeltaCap = 1/40; --Maximum amount a leg's height drive response (pitch, roll) can change in a single tick. Exists to smooth out jerky motion.
-}
+--#################################################################################################################
+--#                                                                                                               #
+--#   This is a general-purpose controller for insectoid walker legs. It should be capable of handling the vast   # 
+--#          majority of walkers with such legs, even some exceptionally weird ones, if set up properly.          #  
+--#                                                                                                               #
+--#   If you do unnatural things to the configuration and perform some modifications, it could probably handle    #
+--#                              humanoid walkers too, though it isn't intended to.                               #               
+--#                                                                                                               #
+--#################################################################################################################
 
 --Terminology note: "Vertical", "lateral", and "medial" (v, l, m) are offsets from the position of a leg's root spinblock, relative to the leg's orientation.
 
 --For properly oriented legs, +v is always up, -v is always down. Lateral and medial depend on which side of the craft a leg is on, but should
 --      follow the rule that +l is the same direction as the side of the craft the leg is on, and -l the opposite. That is, +l should be right and -l should be left
---      for a leg on the right side of the craft, and vice versa for the left. +m should be the direction the leg goes if its hip spinblock rotates in its positive
+--      for a leg on the right side of the craft, and vice versa for the left. +m is the direction the leg goes if its hip spinblock rotates in its positive
 --      direction (which should be clockwise if it's upright). You may want to ensure a positive rotation always means +m by adjusting the hip spinblock's global
 --      response, to make your settings more readable.
 
---You may simply want to fiddle around with positive and negative values depending on the leg to see what gets the kind of motion you want.
+--Basic diagram of a splayed-out leg as viewed from the side. 'O' indicates a lateral spinblock, '[' a vertical one, and '=' is a connecting segment.
+
+--   Ankle   Knee     Hip                        
+--     |       |       |                           
+--     [O======O======O[                               
+--      |             |                          
+--    Foot          Root    
+
+--Hip is attached to the craft, ankle to the sticky foot. Ankle ensures the foot is always pointing in the direction it is moving, foot spinblock ensures it is
+--      always parallel to the ground. Hip, root, and knee joints combine to allow for general articulation.
+
+config = {
+    cycleDuration = 2;          --Amount of time, in seconds, it takes for a single walk cycle to pass.
+    deltaTime = 1/40;           --Amount of time, in seconds, that passes each tick. Should be 1/40th of a second, the length of an FTD physics step.
+    verticalDeltaCap = 1/40;    --Maximum amount total vertical response can change for any given leg in a single tick. Should be on range (0, 1].
+    lateralDeltaCap = 1/40;     --Maximum amount total lateral response can change for any given leg in a single tick. Should be on range (0, 1].
+    medialDeltaCap = 1/40;      --Maximum amount total medial response can change for any given leg in a single tick. Should be on range (0, 1].
+}
 
 --Add an unkeyed table for each leg on the craft into the following table, with these arguments in this order:
 
@@ -56,26 +66,22 @@ config = {
 --forwardResponse:      Response weight to forward drive in each axis. Forward should probably only use the medial axis unless you're doing something weird. {v, l, m}
 --hoverResponse:        Response weight to hover drive in each axis. Hover should probably only use the vertical axis unless you're doing something weird. {v, l, m}
 --strafeResponse:       Response weight to strafe drive in each axis. Strafe should probably only use the lateral axis unless you're doing something weird. {v, l, m}
---rootLength            Length of the segment attached to the root joint spinblock, from the root joint spinblock to (and including) the knee joint spinblock.
---kneeLength            Length of the segment attached to the knee joint spinblock, from the knee joint spinblock to (and including) the foot joint spinblock.
+--stepHeight:           How much the foot raises during a step. Should be positive.
+--rootLength:           Length of the segment attached to the root joint spinblock, from the root joint spinblock to (and including) the knee joint spinblock.
+--kneeLength:           Length of the segment attached to the knee joint spinblock, from the knee joint spinblock to (and including) the foot joint spinblock.
 
 --The total response of a leg in a given axis equals the sum of the request in each drive multiplied by that leg's response to that drive in the given axis, then clamped 
 --    to the range [-1, 1]. This number is then used as a coefficient to determine the direction and length of each step. Technically, a drive response greater than 1 or 
 --    less than -1 is valid, though usually has little practical use.
 
 legSettings = {
-  --{     name, co, restp {v, l, m}, maxp {v, l, m}, minp {v, l, m}, mr {v, l, m}, rr {v, l, m}, pr {v, l, m}, yr {v, l, m},  fr {v, l, m}, hr {v, l, m}, sr {v, l, m}, rl, kl};
-  --{"example",  0,       {0, 0, 0},      {0, 0, 0},      {0, 0, 0},    {0, 0, 0},    {0, 0, 0},    {0, 0, 0},    {0, 0, 0},     {0, 0, 0},    {0, 0, 0},    {0, 0, 0},  0,  0};
+  --{     name, co, restp {v, l, m}, maxp {v, l, m}, minp {v, l, m}, mr {v, l, m}, rr {v, l, m}, pr {v, l, m}, yr {v, l, m},  fr {v, l, m}, hr {v, l, m}, sr {v, l, m}, sh, rl, kl};
+  --{"example",  0,       {0, 0, 0},      {0, 0, 0},      {0, 0, 0},    {0, 0, 0},    {0, 0, 0},    {0, 0, 0},    {0, 0, 0},     {0, 0, 0},    {0, 0, 0},    {0, 0, 0},  0,  0,  0};
 }
 
 
 
---############################################################################
---#                                                                          #
---#   inverseKinematics returns and accepts angles IN RADIANS, NOT DEGREES   #
---#                                                                          #
---############################################################################
-
+--inverseKinematics uses RADIANS, not degrees!
 inverseKinematics = {}
 
 function inverseKinematics.solveExtension(rootLength, kneeLength, distance, angleOffset)
@@ -111,7 +117,7 @@ legController.mt = {__call = legController}
 --I: The "I" variable from the Update() function.
 --For other arguments, see the settings table at the top of the program.
 function legController.new(I, name, restPosition, maxPosition, minPosition, mainResponse, rollResponse, pitchResponse, yawResponse, forwardResponse, hoverResponse, strafeResponse, 
-            rootLength, kneeLength)
+            stepHeight, rootLength, kneeLength)
     local leg = {}
 
     leg.name = name
@@ -140,7 +146,6 @@ end
 
 function legController.actionThread(leg, I)
     local cycleCounter, forwardRequest, yawRequest, mainRequest, walkRequest, pitchRequest, rollRequest, heightRequest, hipAngle, rootAngle, kneeAngle, footAngle, stepLength, heightModifier
-    stepLength = config.stepAngle
     local walkResponse, heightResponse = 0, 0
     
     while true do
